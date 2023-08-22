a466d0911620c02125b33a7ed04e6d7af841967e
Gopistol <rhrjs0131@naver.com>
2023-08-23 04:59:44 +0900

    refactor: 파일 존재 여부 확인하는 로직 추가

--- question_generator.py
+++ question_generator.py
@@ -1,57 +1,40 @@
 import json
-import openai
+import boto3
+import pdfplumber
+from io import BytesIO
 
-openai.api_key = 'My API key'
+s3_client = boto3.client('s3')
+bucket_name = 'mju-interview-bucket-for-lambda'
+file_name = 'test.pdf'
+
 
 def lambda_handler(event, context):
-    # TODO implement
-    content = event['body']
-
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "너는 회사의 면접관이야. 면접자의 자기소개서를 보고 관련 질문 5개를 해줘.",
-            },
-            {
-                "role": "system",
-                "content": "면접자가 어떤 직무를 희망하는지를 유추해서 그 직무와 관련된 질문을 해줘. 서론을 포함하지 말고 질문만 해줘.",
-            },
-            {
-                "role": "system",
-                "content": "질문은 면접자에게 대답을 구하거나 궁금한 점을 물어보는 형식이어야 해. 응답 형식은 1. 질문 내용, 2. 질문 내용... 처럼 해줘.",
-            },
-            {
-                "role": "user",
-                "content": content,
-            }
-        ],
-    )
-
-    response = completion["choices"][0]["message"]["content"]
-
-    response_array = []
-
-    questions = response.strip().split('\n')
-
-    if len(questions) != 5:  # GPT-3.5의 응답으로 받은 질문이 5개가 아닐 경우 재요청
+    s3 = boto3.resource('s3')
+    # 버킷 내 파일이 존재하는지 검사
+    try:
+        s3.Object(bucket_name, file_name).load()
+    except Exception as e:  # 존재하지 않을 때
         return {
-            'statusCode': 502,
-            'body': questions
+            'statusCode': 500,
+            'body': 'file does not exist'
         }
+    else:
+        # test.pdf 파일이 있을 때
+        s3_response = s3_client.get_object(Bucket=bucket_name, Key=file_name)
 
-    for question in questions:
-        elements = question.strip().split('. ')
-        id = elements[0]
-        content = elements[1]
-        response = """{"id":""", id, """"content": """, content, """}"""
-        response_array += response
+        file_data = s3_response["Body"].read()
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(response_array, ensure_ascii=False),
-        "headers": {
-            "Content-Type": "application/json"
-        },
-    }
+        text = ''
+
+        pdf = pdfplumber.open(BytesIO(file_data))
+
+        for page in pdf.pages:
+            text = page.extract_text()
+
+        # 변환 후 파일 삭제
+        s3_client.delete_object(Bucket=bucket_name, Key=file_name)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(text, ensure_ascii=False)
+        }
